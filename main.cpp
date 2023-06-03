7dd91e557683b5f180d5579da911e4de79a6a1bb
nolifer45 <nikolaj.engborg@gmail.com>
2023-06-03 06:48:24 +0200

    Course video No.68

--- main.cpp
+++ main.cpp
@@ -7,20 +7,53 @@ const float speed{0.345};
 Vector2 checkKeys(Vector2 direction)
 {
 
-    if (IsKeyDown(KEY_I)) direction.y += 1;
-    if (IsKeyDown(KEY_K)) direction.y -= 1;
-    if (IsKeyDown(KEY_J)) direction.x += 1;
-    if (IsKeyDown(KEY_L)) direction.x -= 1;
+    if (IsKeyDown(KEY_I))
+        direction.y += 1;
+    if (IsKeyDown(KEY_K))
+        direction.y -= 1;
+    if (IsKeyDown(KEY_J))
+        direction.x += 1;
+    if (IsKeyDown(KEY_L))
+        direction.x -= 1;
     return direction;
 }
-                                                                                                                                                                                                                
-Vector2 updateMapPos(Vector2 mapPos, Vector2 subVector) 
+
+float checkRightLeft(float xDirection)
 {
-    mapPos = Vector2Add(mapPos, Vector2Scale(Vector2Normalize(subVector),speed));
+    float rightLeft{1.f};
+    xDirection > 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+    return rightLeft;
+}
+
+class Character
+{
+public:
+    Vector2 getWorldPosition() { return worldPosition; }
+    void setScreenPosition(int windowWidth, int windowHeight);
+    void tick(float deltaTime);
+
+private:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 screenPosition{};
+    Vector2 worldPosition{};
+    // rightLeft = 1: Right, rightLeft = -1: Left
+    float rightLeft{1.f};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrame{6};
+    const float updateTime{1.f / 12.f};
+    const float speed{2.f};
+};
+
+Vector2 updateMapPos(Vector2 mapPos, Vector2 subVector)
+{
+    mapPos = Vector2Add(mapPos, Vector2Scale(Vector2Normalize(subVector), speed));
     if (mapPos.x > 0)
     {
         mapPos.x = 0;
-
     }
     else if (mapPos.x < -2304.0)
     {
@@ -33,74 +66,79 @@ Vector2 updateMapPos(Vector2 mapPos, Vector2 subVector)
     else if (mapPos.y < -2304.0)
     {
         mapPos.y = -2304.0;
-
     }
     return mapPos;
 }
 
-bool borderCheckMap(Vector2 knightpos){
-    //
+void Character::setScreenPosition(int windowWidth, int windowHeight)
+{
+    screenPosition = {
+        windowWidth / 2 - 8.0F * (0.5F * (float)texture.width / 6),
+        windowHeight / 2 - 8.0F * (0.5F * (float)texture.height)
+    };
 }
+void Character::tick(float deltaTime)
+{
+    Vector2 direction{};
+    if (IsKeyDown(KEY_I))
+        direction.y += 1;
+    if (IsKeyDown(KEY_K))
+        direction.y -= 1;
+    if (IsKeyDown(KEY_J))
+        direction.x += 1;
+    if (IsKeyDown(KEY_L))
+        direction.x -= 1;
+    if (Vector2Length(direction) != 0.0)
+    {
+        worldPosition = Vector2Subtract(worldPosition, Vector2Scale(Vector2Normalize(direction), speed));
+        direction.x < 0.f ? rightLeft = 1.f : rightLeft = -1.f;
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }
+    runningTime += GetFrameTime();
 
-Vector2 updateKnightPos(Vector2 knightPos,Vector2 subVector) {
-    //
-}
-
-float checkRightLeft(float xDirection) {
-    float rightLeft{1.f};
-     xDirection > 0.f ? rightLeft = -1.f : rightLeft = 1.f;
-     return rightLeft;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrame)
+            frame = 0;
+    }
+    // Draw the knight
+    Rectangle source{frame * (float)texture.width / 6, 0.f, rightLeft * (float)texture.width / 6, (float)texture.height};
+    Rectangle dest{screenPosition.x, screenPosition.y, 4.0f * (float)texture.width / 6.0f, 4.0f * (float)texture.height};
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
 }
 int main()
 {
-    const float windowDimensions[2]{768.0,768.0};
-    InitWindow(windowDimensions[0],windowDimensions[1],"slasher!");
+    const float windowDimensionVector[2]{768.0, 768.0};
+    InitWindow(windowDimensionVector[0], windowDimensionVector[1], "slasher!");
 
     Texture2D map = LoadTexture("/Users/Niko/Documents/GameDev/ultimateSlash/nature_tileset/WorldMapv2.png");
-    Vector2 mapPos{};
+    Vector2 mapPosition{};
 
-    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
-    Vector2 knightPos{
-        windowDimensions[0]/2 - 8.0F*(0.5F*(float)knight.width/6),
-        windowDimensions[1]/2 - 8.0F*(0.5F*(float)knight.height)
-    };
+    //Init player character
+    Character knight;
+    knight.setScreenPosition(windowDimensionVector[0],windowDimensionVector[1]);
 
-    float rightLeft{1.f};
-
-    while(!WindowShouldClose())
+    while (!WindowShouldClose())
     {
-        //Start draw window
+        // Start draw window
         BeginDrawing();
         ClearBackground(WHITE);
-        Vector2 direction{};
 
+        mapPosition = Vector2Scale(knight.getWorldPosition(),-1.f);
         
-        direction = checkKeys(direction);
+        // Draw the map
+        DrawTextureEx(map, mapPosition, 0.0, 4.0, WHITE);
+        knight.tick(GetFrameTime());  
 
-
-        if (Vector2Length(direction) != 0.0)
-        {
-            mapPos = updateMapPos(mapPos,direction,speed);
-            //Set direction left/right
-            if (direction.x != 0) {
-                rightLeft = checkRightLeft(direction.x);
-            }
-        }
-       
-        
-        
-        
-        //Draw the map      
-        DrawTextureEx(map,mapPos,0.0,4.0,WHITE);
-        
-        //Draw the knight
-        Rectangle source {0.f,0.f, rightLeft * (float)knight.width/6 ,(float)knight.height};
-        Rectangle dest {knightPos.x,knightPos.y,4.0f*(float)knight.width/6.0f,4.0f*(float)knight.height};
-        DrawTexturePro(knight,source,dest,Vector2{},0.f,WHITE);
-        
         EndDrawing();
     }
-    //Unload program assets 
+    // Unload program assets
     CloseWindow();
     UnloadTexture(map);
 }
